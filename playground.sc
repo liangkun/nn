d846634c4bff5070f18b06fecbbede59b84f28ed
liangkun <liangkun@outlook.com>
2014-11-16 21:44:16 +0800

    a little code motion

--- playground.sc
+++ playground.sc
@@ -8,25 +8,4 @@ import org.sann.algos.Perceptron.ListErrorCollector
 import org.sann.data.gen.DoubleMoon
 import org.sann._
 
-val size = 1000
-val train = DoubleMoon.gen(size, -2)
-val xs = DenseMatrix.horzcat(DenseMatrix.ones[Float](size, 1), train(::, 0 to 1))
-val ys = train(::, 2)
-val weights = DenseVector.zeros[Float](3)
-val ecollector = new ListErrorCollector
-val (iters, error) = Perceptron.train(
-  weights,
-  xs,
-  ys,
-  maxIters = 1000,
-  learningRate = 1f,
-  pocket = true,
-  errorCollector = Some(ecollector)
-)
-val fig = new Figure("Playground")
-val plt = fig.subplot(1, 2, 0)
-utils.plotBinaryDataPoints(train).foreach(s => plt += s)
-plt += utils.plotLine(weights, -14.0f, 24.0f)
-val plt1 = fig.subplot(1, 2, 1)
-plt1 += utils.plotLearningCurve(ecollector.get)
 
--- src/main/scala/org/sann/Main.scala
+++ src/main/scala/org/sann/Main.scala
@@ -3,12 +3,14 @@
  */
 package org.sann
 
+
 import scala.io.Source
 
 import breeze.linalg.{DenseMatrix, DenseVector}
 import breeze.plot.Figure
 
 import org.sann.algos.Perceptron
+import org.sann.algos.Perceptron.ListErrorCollector
 import org.sann.data.gen.DoubleMoon
 import org.sann.data.io
 import org.sann.utils.plotBinaryDataPoints
@@ -16,13 +18,47 @@ import org.sann.utils.shuffleRows
 
 object Main {
   def main(args: Array[String]): Unit = {
-
+    nnChp1()
   }
 
   def plotDoubleMoon(size: Int = 1000, d: Float = 0.0f): Unit = {
     val fig = new Figure("DoubleMoon")
     val plt = fig.subplot(0)
     plotBinaryDataPoints(DoubleMoon.gen(size, d)).foreach(s => plt += s)
+  }
+
+  def nnChp1(): Unit = {
+    val d = -4
+    val trainSize = 5000
+    val train = DoubleMoon.gen(trainSize, d)
+    val trainXs = DenseMatrix.horzcat(DenseMatrix.ones[Float](trainSize, 1), train(::, 0 to 1))
+    val trainYs = train(::, 2)
+    val weights = DenseVector.zeros[Float](3)
+    val errorCollector = new ListErrorCollector
+    val (iters, trainErrors) = Perceptron.train(
+      weights,
+      trainXs,
+      trainYs,
+      maxIters = 1000,
+      learningRate = 1f,
+      pocket = true,
+      errorCollector = Some(errorCollector)
+    )
+    val fig = new Figure("Playground")
+    val plt = fig.subplot(1, 2, 0)
+    utils.plotBinaryDataPoints(train).foreach(s => plt += s)
+    plt += utils.plotLine(weights, -14.0f, 24.0f)
+    val plt1 = fig.subplot(1, 2, 1)
+    plt1 += utils.plotLearningCurve(errorCollector.get)
+    println(s"Train Error Rate($trainErrors/$trainSize) = ${trainErrors.toFloat / trainSize}")
+
+    // test
+    val testSize = 2000
+    val test = DoubleMoon.gen(testSize, d)
+    val testXs = DenseMatrix.horzcat(DenseMatrix.ones[Float](testSize, 1), test(::, 0 to 1))
+    val testYs = test(::, 2)
+    val testErrors = Perceptron.countErrors(weights, testXs, testYs)
+    println(s"Test Error Rate($testErrors/$testSize) = ${testErrors.toFloat / testSize}")
   }
 
   def mlfHw(): Unit = {
