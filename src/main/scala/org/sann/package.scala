32d6adb143e9ff6021811fdba59534bca5c0e28b
liangkun <liangkun@outlook.com>
2014-12-16 08:45:23 +0800

    temp version

--- src/main/scala/org/sann/package.scala
+++ src/main/scala/org/sann/package.scala
@@ -10,47 +10,70 @@ import scala.language.implicitConversions
 import breeze.linalg._
 
 package object sann {
-  /** Type of an activating function. */
-  type Activator = DenseVector[Double] => DenseVector[Double]
-
-  /**
-   * Type of weights updating (learning) function.
-   * (current weights, current input, current error, learner private info) => learner private info
-   */
-  type Learner =
-    (DenseMatrix[Double], DenseVector[Double], DenseVector[Double], Option[Any]) => Option[Any]
+  /** Type of an activating function (vectorized). */
+  case class Activator(evaluate: (DenseVector[Double] => DenseVector[Double]),
+                       derivate: (DenseVector[Double] => DenseVector[Double]))
 
   /**
    * A group of isomorphism neurons.
    *
    * @param cardinality number of neurons in this group.
    * @param activator activating function of the neurons.
-   * @param inputs synapses that provide inputs to each of the neuron in this group.
+   * @param inputs synapses that provide inputs to each of the neuron in this group, order is important.
    */
-  case class Neurons(cardinality: Int, activator: Activator, inputs: List[Synapses] = List())
-
-  /**
-   * A group of synapses that connects every input neurons to every output neurons.
-   *
-   * @param input input neurons.
-   * @param updater update function of the weights.
-   */
-  case class Synapses(input: Neurons, updater: Learner)
-
-  // =============================================================================================
-  // Neural Networks DSL Definition.
-  // =============================================================================================
-  case class SannNode(self: Neurons) {
-    def ->(neurons: Neurons): Neurons = Neurons(
-      neurons.cardinality,
-      neurons.activator,
-      Synapses(self, perceptronLearner)::neurons.inputs
-    )
+  case class Neurons(cardinality: Int, activator: Activator, inputs: List[Synapses] = List()) {
+    def ->(neurons: Neurons) = neurons.copy(inputs = Synapses(this) :: neurons.inputs)
   }
 
-  def inputs(size: Int) = Neurons(size, linearActivator, List())
+  /**
+   * A group of synapses that have the input neurons connected as input.
+   *
+   * @param input input neurons.
+   */
+  case class Synapses(input: Neurons)
 
-  implicit def neuronsToSannNode(neurons: Neurons): SannNode = SannNode(neurons)
+  /**
+   * Input (from external) neurons of the network.
+   *
+   * @param cardinality of the input neurons.
+   */
+  def inputs(cardinality: Int) = Neurons(cardinality, linearActivator, List())
+
+  /** Linear(identity) Activator */
+  val linearActivator = Activator(
+    evaluate = x => x,
+    derivate = x => DenseVector.ones(x.length)
+  )
+
+  /**
+   * Compiled Sann with compact internal representation, ready for training and working.
+   */
+  class CompiledSann {
+
+  }
+
+  def compile(output: Neurons): CompiledSann = {
+    val neuronsCount = countCascade(output)
+    ???
+  }
+
+  // count all the neurons that can reach the specified neurons.
+  private def countCascade(neurons: Neurons, seen: Set[Neurons] = Set()): Int = {
+    if (seen.contains(neurons)) {
+      0
+    } else {
+      var count = 1
+      val updatedSeen = seen + neurons
+      for (input <- neurons.inputs) {
+        count += countCascade(input.input, updatedSeen)
+      }
+      count
+    }
+  }
+
+  // =============================================================================================
+  // Internal
+  // =============================================================================================
 
   def perceptronLearner(
     weights: DenseMatrix[Double],
@@ -62,77 +85,4 @@ package object sann {
 
     None
   }
-
-  // =============================================================================================
-  // Compiled neural network
-  // =============================================================================================
-  class CompiledSnn {
-    var weights: List[DenseMatrix[Double]] = List()
-    var learners: List[Learner] = List()
-    var activators: List[Activator] = List()
-    private[this] var _inputs: List[DenseVector[Double]] = List()
-
-    def train(trainSet: Iterable[(DenseVector[Double], DenseVector[Double])]):Unit = {
-      var done = false
-
-      while(!done) {
-        var hasError = false
-        for ((input, expected) <- trainSet) {
-          val output = ff(input)
-          var error = 0.0
-          if (output(0) > 0 && expected(0) < 0) {
-            error = -1.0
-          } else if (output(0) < 0 && expected(0) > 0) {
-            error = 1.0
-          }
-          if (error != 0.0) {
-            hasError = true
-            bp(DenseVector(error))
-          }
-        }
-        done = !hasError
-      }
-    }
-
-    def ff(input: DenseVector[Double]): DenseVector[Double] = {
-      _inputs = List(input)
-      for ((w, f) <- weights.zip(activators)) {
-        val output = f(_inputs.head)
-        _inputs = (w.t * output) :: _inputs
-      }
-      _inputs.head
-    }
-
-    def bp(error: DenseVector[Double]): Unit = {
-      val ws = weights.reverse.tail
-      val ls = learners.reverse
-      val ins = _inputs.tail
-      for (i <- 0 until ws.size) {
-        ls(i)(ws(i), ins(i), error, None)
-      }
-    }
-  }
-
-  def compile(output: Neurons): CompiledSnn = {
-    val result = new CompiledSnn
-
-    if (output.inputs.size == 1) {
-      val synapses = output.inputs(0)
-      val subgraph = compile(synapses.input)
-      result.weights = subgraph.weights ++ List(DenseMatrix.rand[Double](synapses.input.cardinality, output.cardinality), DenseMatrix.eye[Double](output.cardinality))
-      result.activators = subgraph.activators :+ output.activator
-      result.learners = subgraph.learners :+ synapses.updater
-    }
-    result
-  }
-
-  // =============================================================================================
-  // Common Activator definitions.
-  // =============================================================================================
-
-  def linearActivator(zs: DenseVector[Double]): DenseVector[Double] = zs
-
-  // =============================================================================================
-  // developer APIs.
-  // =============================================================================================
 }
